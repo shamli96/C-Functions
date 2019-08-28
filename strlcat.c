e40e28a8365ce09c49b6ab0abd549149caa13e35
Shamli <sherlybadole96@gmail.com>
2019-08-28 14:31:36 +0530

    stlcat provides the way to check if destination string is truncated if dest size is smaller

---
+++ strlcat.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+#include <bsd/string.h>
+
+int main()
+{	
+	const char somestring[] = "bar";
+	char buf[5] = "foo";
+	size_t len;
+
+	len=strlcat(buf, somestring, sizeof(buf)) ;
+	if (len >= sizeof(buf))
+		printf("somestring was truncated, when concatenating to buf.\n");
+	else
+		printf("String is concatenated properly. Resultant string is :%s\n",buf);
+	printf("strlcat has returned = %zd bytes\n",len);
+	printf("Size of the destination string is :%zd",strlen(buf));
+	return 0;
+}
